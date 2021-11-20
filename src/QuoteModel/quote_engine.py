ef77836fd4ae9ac7696a73877e25c511b8b2186e
RasNath <nathan.f.rasmussen@gmail.com>
2021-11-19 19:38:02 -0800

    add TXTIngestor

--- src/QuoteModel/quote_engine.py
+++ src/QuoteModel/quote_engine.py
@@ -30,6 +30,30 @@ class IngestorInterface(ABC):
         pass
 
 
+class TXTIngestor(IngestorInterface):
+    allowed_extensions = ['txt']
+
+    @classmethod
+    def parse(cls, path: str) -> List[QuoteModel]:
+        if not cls.can_ingest(path):
+            raise Exception('cannot ingest exception')
+
+        quotes = []
+
+        with open(path, "r") as infile:
+            data = infile.readlines()
+
+        for d in data:
+            d_ = d.strip()
+            d_split = d_.split(" - ")
+            quote = d_split[0]
+            author = d_split[1]
+            new_quote = QuoteModel(quote, author)
+            quotes.append(new_quote)
+
+        return quotes
+
+
 class CSVIngestor(IngestorInterface):
     allowed_extensions = ['csv']
 
@@ -101,7 +125,7 @@ class PDFIngestor(IngestorInterface):
 
 
 class Ingestor(IngestorInterface):
-    ingestors = [DocxIngestor, CSVIngestor, PDFIngestor]
+    ingestors = [TXTIngestor, DocxIngestor, CSVIngestor, PDFIngestor]
        
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
