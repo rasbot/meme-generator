ac5bfe96f2637ce5477599185a7ab1b886183c26
rasbot <nathan.f.rasmussen@gmail.com>
2021-11-27 00:28:35 -0800

    finish docstrings, fix pylint issues

--- src/meme.py
+++ src/meme.py
@@ -1,3 +1,10 @@
+"""Meme module used to generate memes locally.
+
+Create a meme using dog or cat pictures, along
+with normally generated (human) quotes, or machine
+learning generated (gpt-2 model) quotes. Optionally,
+create memes with custom text and authors. Saves file
+to a local directory."""
 import os
 import random
 import argparse
@@ -6,55 +13,109 @@ from QuoteModel.quote_engine import Ingestor, QuoteModel
 from MemeEngine.meme_engine import MemeEngine
 
 
-def generate_meme(path='dog', body=None, author=None):
-    """ Generate a meme given a path and a quote """
+def generate_meme(
+        path: str = "./tmp",
+        animal_type: str = "dog",
+        body: str = "",
+        author: str = "",
+        quote_type: str = "normal",
+) -> str:
+    """Generate a random meme using custom or ingested quotes.
 
-    if path == 'dog':
-        image_path = "./_data/photos/dog/"
-    else:
-        image_path = "./_data/photos/cat/"
+    Uses a random image of a dog or a cat, and either a random
+    quote / author, or a user-defined quote. Saves image to a
+    local directory.
+
+    Args:
+        path (str, optional): Path to save meme to.
+        animal_type (str, optional): Type of animal (dog or cat).
+            Defaults to 'dog'.
+        body (str, optional): Quote string. Defaults to "".
+        author (str, optional): Author string. Defaults to "".
+        quote_type (str, optional): Type of quote (normal or gpt2).
+            Defaults to 'normal'.
+
+    Raises:
+        Exception: Raise exception if body is provided but
+            author is not provided.
+
+    Returns:
+        str: Path to meme generated.
+    """
     imgs = []
-    for root, dirs, files in os.walk(image_path):
+    for root, _, files in os.walk(f"./_data/photos/{animal_type}/"):
         imgs = [os.path.join(root, name) for name in files]
 
     img = random.choice(imgs)
 
-    if body is None:
-        if author == 'gpt2':
-            folder = 'gpt2'
+    if not body:
+        if animal_type == "dog":
+            animal = "Dog"
+        elif animal_type == "cat":
+            animal = "Cat"
         else:
-            folder = 'normal'
-        if path == 'dog':
-            animal = 'Dog'
-        else:
-            animal = 'Cat'
-        quote_files = [f'./_data/{animal}Quotes/{folder}/{animal}QuotesTXT.txt',
-                    f'./_data/{animal}Quotes/{folder}/{animal}QuotesDOCX.docx',
-                    f'./_data/{animal}Quotes/{folder}/{animal}QuotesPDF.pdf',
-                    f'./_data/{animal}Quotes/{folder}/{animal}QuotesCSV.csv']
+            raise Exception("Animal type not supported. Use dog or cat.")
+        quote_files = [
+            f"./_data/{animal}Quotes/{quote_type}/{animal}QuotesTXT.txt",
+            f"./_data/{animal}Quotes/{quote_type}/{animal}QuotesDOCX.docx",
+            f"./_data/{animal}Quotes/{quote_type}/{animal}QuotesPDF.pdf",
+            f"./_data/{animal}Quotes/{quote_type}/{animal}QuotesCSV.csv",
+        ]
         quotes = []
-        for f in quote_files:
-            quotes.extend(Ingestor.parse(f))
+        for quote_file in quote_files:
+            quotes.extend(Ingestor.parse(quote_file))
 
         quote = random.choice(quotes)
     else:
-        if author is None:
-            raise Exception('Author Required if Body is Used')
+        if not author:
+            raise Exception("Author Required if Body is Used")
         quote = QuoteModel(body, author)
 
-    meme = MemeEngine('./tmp')
-    path = meme.make_meme(img, quote.body, quote.author)
-    return path
+    return MemeEngine(path).make_meme(img, quote.body, quote.author)
 
 
 if __name__ == "__main__":
-    # @TODO Fix 'path' variable info
-    # text still spilling off edge of image
-    parser = argparse.ArgumentParser(description='Create a meme using a photo, a quote, and an author.')
-    parser.add_argument('--path', type=str, default=None, help='path to image file')
-    parser.add_argument('--body', type=str, default=None, help='quote to add to image')
-    parser.add_argument('--author', type=str, default=None, help='author of quote to add to image')
-    
-    args = parser.parse_args()
-    # print(generate_meme("./_data/photos/cat/", "I'm a kitty", "Mr. Snickers"))
-    print(generate_meme(args.path, args.body, args.author))
+    PARSER = argparse.ArgumentParser(
+        description="Create a meme using a photo, a quote, and an author."
+    )
+    PARSER.add_argument(
+        "--path",
+        type=str,
+        default="",
+        help="path to image file"
+    )
+    PARSER.add_argument(
+        "--animal",
+        type=str,
+        default="dog",
+        help="animal used for meme (dog or cat)"
+    )
+    PARSER.add_argument(
+        "--type",
+        type=str,
+        default="normal",
+        help="type of quote to use (normal or gpt2)",
+    )
+    PARSER.add_argument(
+        "--body",
+        type=str,
+        default="",
+        help="quote to add to image"
+    )
+    PARSER.add_argument(
+        "--author",
+        type=str,
+        default="",
+        help="author of quote to add to image"
+    )
+
+    ARGS = PARSER.parse_args()
+    print(
+        generate_meme(
+            ARGS.path,
+            ARGS.animal,
+            ARGS.body,
+            ARGS.author,
+            ARGS.type
+        )
+    )
